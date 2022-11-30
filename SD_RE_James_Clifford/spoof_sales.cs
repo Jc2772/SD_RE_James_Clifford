28b69a166ebb352756da5b121607cc79b1213cef
T00225039 <james.clifford2@students.ittralee.ie>
2022-11-30 16:13:24 +0000

    .

---
+++ SD_RE_James_Clifford/spoof_sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD_RE_James_Clifford
+{
+    class spoof_sales
+    {
+        private List<string> buyer_name = new List<string> { };
+        private List<string> final_price = new List<string> { };
+        private List<string> buyer_contact = new List<string> { };
+    }
+}
