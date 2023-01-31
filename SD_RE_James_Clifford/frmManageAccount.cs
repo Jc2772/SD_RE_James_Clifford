c96b1ae632852222da55e3e9d45117d671da025d
T00225039 <james.clifford2@students.ittralee.ie>
2023-01-31 12:42:23 +0000

    added update

--- SD_RE_James_Clifford/frmManageAccount.cs
+++ SD_RE_James_Clifford/frmManageAccount.cs
@@ -91,43 +91,51 @@ namespace SD_RE_James_Clifford
          public void checkUpdate(String Name, String Address1, String Address2, String Address3, String Phone, String Email, int id)
         {
             List<string> values = new List<string> { Name, Address1, Address2, Address3, Phone, Email };
-            for (int i = 0; i < 5; i++)
+            DataSet dataset = accounts.GetData(id);
+            foreach (DataRow row in dataset.Tables[0].Rows)
             {
-                if (string.IsNullOrEmpty(values[i]))
+                int Counts = 0;
+                foreach (DataColumn column in dataset.Tables[0].Columns)
                 {
-                    switch (i)
+                    if(Counts >= 1 && Counts <= 6)
                     {
-                        case 0:
+                        if (String.IsNullOrEmpty(values[Counts - 1])){
+                            switch (Counts)
                             {
-                                Name = accounts.getAccName()[id-1];
-                                break;
+                                case 1:
+                                    {
+                                        Name = row[column].ToString();
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Address1 = row[column].ToString();
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        Address2 = row[column].ToString();
+                                        break;
+                                    }
+                                case 4:
+                                    {
+                                        Address3 = row[column].ToString();
+                                        break;
+                                    }
+                                case 5:
+                                    {
+                                        Phone = row[column].ToString();
+                                        break;
+                                    }
+                                case 6:
+                                    {
+                                        Email = row[column].ToString();
+                                        break;
+                                    }
                             }
-                        case 1:
-                            {
-                                Address1 = accounts.getAccAddress1()[id - 1];
-                                break;
-                            }
-                        case 2:
-                            {
-                                Address2 = accounts.getAccAddress2()[id - 1];
-                                break;
-                            }
-                        case 3:
-                            {
-                                Address3 = accounts.getAccAddress3()[id - 1];
-                                break;
-                            }
-                        case 4:
-                            {
-                                Phone = accounts.getAccPhone()[id - 1];
-                                break;
-                            }
-                        case 5:
-                            {
-                                Email = accounts.getAccEmail()[id - 1];
-                                break;
-                            }
+                        }
                     }
+                    Counts++;
                 }
             }
             accounts.updateValues(Name, Address1, Address2, Address3, Phone, Email ,id);
--- SD_RE_James_Clifford/spoof_accounts.cs
+++ SD_RE_James_Clifford/spoof_accounts.cs
@@ -130,31 +130,40 @@ namespace SD_RE_James_Clifford
         }
         public void updateValues(string name, string address1, string address2, string address3, string phone, string email,int id)
         {
-            String query = "UPDATE Owners Set OwnerName = '" + name + "',Area = '" + address1 + "',Town = '" + address2 + "',County = '" + address3 + "',PhoneNo = '" + phone + "',Email = '" + email + "'WHERE " + id + " = OwnerId;" ;
+            String query = "UPDATE Owners Set OwnerName = '" + name + "',Area = '" + address1 + "',Town = '" + address2 + "',County = '" + address3 + "',PhoneNo = '" + phone + "',Email = '" + email + "'WHERE " + id + " = OwnerId" ;
             OracleCommand cmd = new OracleCommand(query, connection);
             cmd.ExecuteNonQuery();
         }
         public void removeAccounts(int id)
         {
-            String query = "UPDATE Owners SET Status = 'D' WHERE OwnerId = " + id + ";"; 
+            String query = "UPDATE Owners SET Status = 'D' WHERE OwnerId = " + id; 
             OracleCommand cmd = new OracleCommand(query, connection);
             cmd.ExecuteNonQuery();
         }
         public void reinstateAccount(String Phone)
         {
-            String query = "UPDATE Owners Set Status := 'R' WHERE Email =" + Phone + ";";
+            String query = "UPDATE Owners Set Status := 'R' WHERE PhoneNo ='" + Phone + "'";
             OracleCommand cmd = new OracleCommand(query, connection);
             cmd.ExecuteNonQuery();
         }
         public string getUserStatus(String Phone)
         {
-            String query = "SELECT Status FROM OWNERS WHERE PhoneNo = '" +  Phone + "';";
+            String query = "SELECT Status FROM OWNERS WHERE PhoneNo = '" +  Phone + "'";
             OracleCommand cmd = new OracleCommand(query, connection);
             OracleDataAdapter dataAdapter = new OracleDataAdapter(cmd);
             DataSet dataset = new DataSet();
             dataAdapter.Fill(dataset);
             String status = dataset.Tables[0].Rows[0]["Status"].ToString(); ;
             return status;
+        }
+        public DataSet GetData(int id)
+        {
+            String query = "SELECT * FROM OWNERS WHERE OwnerId = " + id;
+            OracleCommand cmd = new OracleCommand(query, connection);
+            OracleDataAdapter dataAdapter = new OracleDataAdapter(cmd);
+            DataSet dataset = new DataSet();
+            dataAdapter.Fill(dataset);
+            return dataset;
         }
     }
 }
