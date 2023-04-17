f5e88229276fb3ec5d311b04a68dea543a62aeb7
T00225039 <james.clifford2@students.ittralee.ie>
2023-04-17 17:14:01 +0100

    .

--- SD_RE_James_Clifford/auction.cs
+++ SD_RE_James_Clifford/auction.cs
@@ -54,13 +54,11 @@ namespace SD_RE_James_Clifford
         public int GetAuctionId(DateTime auctiondate)
         {
             string query = "SELECT AuctionId FROM Auctions where auctiondate = '" + auctiondate.Date.ToString("dd-MMM-yyy") + "'";
-            connection.Open();
             OracleCommand cmd = new OracleCommand(query, connection);
             OracleDataAdapter dataAdapter = new OracleDataAdapter(cmd);
             DataSet dataset = new DataSet();
             dataAdapter.Fill(dataset);
             int id = Convert.ToInt32(dataset.Tables[0].Rows[0][0].ToString());
-            connection.Close();
             return id ;
         }
         public List<DateTime> GetAuctionDates()
@@ -96,11 +94,11 @@ namespace SD_RE_James_Clifford
         }
         public void addBooking(int id, double price,string timeslot,DateTime date,String TagNo)
         {
-            string query = "INSERT INTO Bookings(BookingId,AuctionId,timeslot,OwnerId,StartingPrice,TagNo) Values(" + nextBookingId() + "," + GetAuctionId(date) + ",'" + timeslot  + "'," + id + "," + price +"," + TagNo  + ")";
             connection.Open();
+            string query = "INSERT INTO Bookings(BookingId,AuctionId,timeslot,OwnerId,StartingPrice,TagNo) Values(" + nextBookingId() + "," + GetAuctionId(date) + ",'" + timeslot  + "'," + id + "," + price +"," + TagNo  + ")";
             OracleCommand cmd = new OracleCommand(query, connection);
+            cmd.ExecuteNonQuery(); 
             connection.Close();
-            cmd.ExecuteNonQuery();
         }
         public List<string> getTimes()
         {
@@ -134,9 +132,7 @@ namespace SD_RE_James_Clifford
         private int nextBookingId()
         {
             string query = "Select MAX(BookingId) from Bookings";
-            connection.Open();
             OracleCommand cmd = new OracleCommand(query, connection);
-            connection.Close();
             OracleDataReader data = cmd.ExecuteReader();
             data.Read();
             if (data.IsDBNull(0))
